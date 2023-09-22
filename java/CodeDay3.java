5c82d35a871a355dde0cff5262a2f092e40676e5
thousifameerkhan <thousifameerkhan@gmail.com>
2023-09-22 16:45:02 +0530

    day2

---
+++ java/CodeDay3.java
@@ -0,0 +1,22 @@
+class CodeDay3{
+
+    CodeDay3(){}
+    public static void main(String[] args){
+        System.out.println("Code day 3");
+
+        int i = 5 ;
+
+        int[] arrays1 = new int[i];
+
+        arrays1[0] = 570;
+
+        // for each loop in java
+        for(int k : arrays1)
+        {
+
+            System.out.println(k);
+        }
+
+
+    }
+}
