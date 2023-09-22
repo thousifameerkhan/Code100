fbc5bf535d6711716158913bfcd89c85cd748ac4
thousifameerkhan <thousifameerkhan@gmail.com>
2023-09-22 12:09:26 +0530

    day2

---
+++ java/CodeDay2.java
@@ -0,0 +1,16 @@
+//import java.lang.*;
+
+class CodeDay2 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("Hello World");
+        System.out.println("Hello World 2");
+
+        int[] a = new int[10];
+
+        a[0] = 150;
+
+        System.out.println(a[0]);
+    }
+}
